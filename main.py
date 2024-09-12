682703634d6eaa04c464dfd6e0d70df94ccb15a4
oid85 <oid85@mail.ru>
2024-09-12 14:30:50 +0700

    изменил параметры

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def do():
     while True:
         pyautogui.moveTo(1000, 500)
         pyautogui.click()
-        time.sleep(5)
+        time.sleep(10)
         pyautogui.moveTo(1500, 700)
         pyautogui.click()
-        time.sleep(60)
+        time.sleep(30)
 
 
 if __name__ == '__main__':
